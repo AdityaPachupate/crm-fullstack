362b1c4273fae42944e5388b556f45c16cb64d2c
Aditya Pachupate <adityapachupate@gmail.com>
2026-04-10 18:47:12 +0530

    feat: add database migration to update schema for Neon DB compatibility

---
+++ CRM.API/Infrastructure/Persistence/Migrations/20260410131525_UpdateNeonDb.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.API.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNeonDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "RejoinRecords");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "LookupValues");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "FollowUps");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Bills");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "RejoinRecords",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "LookupValues",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "FollowUps",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Enrollments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Bills",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000001"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000002"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000003"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000004"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000005"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000006"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000007"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LookupValues",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-0000-0000-000000000008"),
+                column: "UpdatedAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
